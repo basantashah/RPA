edf4efba881f4f998ea8b980b85edc4cc05acc74
Basant Shah <basantashah1993@gmail.com>
2024-06-04 15:35:20 +0545

    adding code to play recording

---
+++ play.py
@@ -0,0 +1,40 @@
+import time
+import pyautogui
+
+time.sleep(1)
+
+pyautogui.press('win')
+time.sleep(1)
+
+pyautogui.press('f')
+time.sleep(0.22220540046691897)
+
+pyautogui.press('i')
+time.sleep(0.10727632045745851)
+
+pyautogui.press('r')
+time.sleep(0.08800437450408936)
+
+pyautogui.press('e')
+time.sleep(0.5824827909469605)
+
+pyautogui.press('f')
+time.sleep(0.11989445686340333)
+
+pyautogui.press('o')
+time.sleep(0.22220461368560793)
+
+pyautogui.press('x')
+time.sleep(2.674463224411011)
+
+pyautogui.moveTo(206, 219)
+pyautogui.mouseDown()
+time.sleep(0.07921419143676758)
+
+pyautogui.moveTo(206, 219)
+pyautogui.mouseUp()
+time.sleep(5.592976307868958)
+
+pyautogui.moveTo(522, 68)
+pyautogui.mouseDown()
+time.sleep(0.11439170837402345)
